a171781aa8ea107a86717b1a1966ae74555cdc39
Maria Feher-Lehrner <maria@feher.at>
2024-10-09 21:47:03 +0200

    Added function readDouble to convert a string input into an numerical value.
    Added function calculate EquityAfterInterest to calculate capital with compound interest.

--- Zinsenrechner/src/Main.java
+++ Zinsenrechner/src/Main.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
     }
+
+    // Jetzt brauchen wir noch eine Funktion, die uns das Risikolevel in einen Zinssatz umwandelt. Evtl. ein switch mit cases?
+
+    private static double calculateEquityAfterInterest(double initialCapital, int duration, double interestRate) {
+        double finalCapital = initialCapital * Math.pow((1 + interestRate/100), duration);
+        return finalCapital;
+    }
+
+    private static double readDouble(String caption) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(caption);
+        return scanner.nextDouble();
+    }
+
 }
