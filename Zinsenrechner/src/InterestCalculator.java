ff56eea54ce989e1e8ed4bf15c2b526234a5fd0f
Maria Feher-Lehrner <maria@feher.at>
2024-11-29 18:43:45 +0100

    testing/ Analyse und Design: Definierung logische Testfälle für calculateEquityAfterInterest()
    testing/ Realisierung: Definierung konkrete Testfälle für calculateEquityAfterInterest()

--- Zinsenrechner/src/InterestCalculator.java
+++ Zinsenrechner/src/InterestCalculator.java
@@ -1,7 +1,7 @@
 public class InterestCalculator {
 
     //***Method is used in Main to determine the interest rate based on the risk class input***
-    double fetchInterestRate(int risk){
+    double determineInterestRate(int risk){
         double interestRate = switch (risk) {
             case 1 -> 2.5;
             case 2 -> 7;
@@ -13,7 +13,22 @@ public class InterestCalculator {
     }
 
     //***Method is used in Main to calculate the final max-capital after interest gains.***
+
+    //+++initialCapital+++
+    //1) gültige Äquivalenzklasse: 100.00 bis 10000000.00 --> Konkrete Testfälle: 99.99, 100.00, 100.01, 9999999.99, 10000000.00, 10000000.01, mehr als 2 Nachkommastellen werden in der Eingabe aber akzeptiert
+    //2) ungültige Äquivalenzklasse: alles, was sich nicht in diesem Bereich befindet
+
+    //+++duration+++
+    //1) gültige Äquivalenzklasse: 1 bis 30 Jahre --> Konkrete Testfälle: 0, 1, 2, 29, 30, 31
+    //2) ungültige Äquivalenzklasse: alles, was sich nicht in diesem Bereich befindet.
+
+    //+++interestRate+++
+    //1) gültige Äquivalenzklasse: [2.5, 7, 15] --> Konkrete Testfälle: 2, 2.5, 5, 7, 10.5, 15, 15.5
+    //2) ungültige Äquivalenzklasse: jede Zahl, die NICHT 2.5, 7 oder 15 ist.
+
     double calculateEquityAfterInterest(double initialCapital, int duration, double interestRate) {
+
+        //TODO: Hier einen Check einbauen, dass nur Zahlen >=100 und <=10.000.000!
         double finalCapital = initialCapital * Math.pow((1 + interestRate/100), duration);
         return finalCapital;
     }
--- Zinsenrechner/src/Main.java
+++ Zinsenrechner/src/Main.java
@@ -1,17 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) throws wrongInputException {
 
-        InputScanner inputScanner = new InputScanner();
+        Scanner scanner = new Scanner(System.in);
         InterestCalculator interestCalculator = new InterestCalculator();
 
         //TODO: Refactor into loop for new calculations until user ends loop
 
-        double initialCapital = inputScanner.readDouble("How much money (in Euro) would you like to invest?");
-        int duration = inputScanner.readInt("How long would you like to invest that money for (in years)?");
-        int risk = inputScanner.readInt("How risky should your investment be? Please enter 1 for low risk (interest rate of 2.5 percent), 2 for medium risk (interest rate of 7 percent) or 3 for high risk (interest rate of 15 percent).");
+        System.out.println("How much money (in Euro) would you like to invest?");
+        double initialCapital = scanner.nextDouble();
+        System.out.println("How long would you like to invest that money for (in years)?");
+        int duration = scanner.nextInt();
+        System.out.println("How risky should your investment be? Please enter 1 for low risk (interest rate of 2.5 percent), 2 for medium risk (interest rate of 7 percent) or 3 for high risk (interest rate of 15 percent).");
+        int risk = scanner.nextInt();
 
-        double interestRate = interestCalculator.fetchInterestRate(risk);
+        double interestRate = interestCalculator.determineInterestRate(risk);
         double finalEquity = interestCalculator.calculateEquityAfterInterest(initialCapital, duration, interestRate);
 
         System.out.printf("If you invest %.2f Euro for %d years with an interest rate of %.2f, your final equity could be up to %.2f.",
