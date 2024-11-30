f921d73d32f4253c89d1d2438661f2828a3307d6
Maria Feher-Lehrner <maria@feher.at>
2024-11-30 20:48:29 +0100

    Changes by Lisa

--- Zinsenrechner/src/InterestCalculator.java
+++ Zinsenrechner/src/InterestCalculator.java
@@ -22,13 +22,7 @@ public class InterestCalculator {
 
 
     //***Method is used to validate the desired max and min value of the initial capital.***
-    //+++initialCapital+++
-    //1) valid EC: Double with two digits after the decimal point, ranging from 100.00 to 10000000.00
-    // --> Test Cases - concrete values: 99.99, 100.00, 100.01, 9999999.99, 10000000.00, 10000000.01
-    // More than two digits after the decimal point can be entered too (but should be invalidated later)
-    //2) invalid EC: everything outside of this range --> --> Test cases already covered in valid EC
-
-    double validateInitialCapital (double initialCapital) throws wrongInputException {
+    double validateInitialCapital(double initialCapital) throws wrongInputException {
         BigDecimal validatedCapital = BigDecimal.valueOf(initialCapital);
         if (validatedCapital.scale() > 2 || initialCapital < 100 || initialCapital > 10000000) {
             throw new wrongInputException("The capital to invest must be a value between 100,00 and 10.000.000,00 EURO and must not have more than 2 decimals.");
