c5b9e35232ff329ada9da827783460b5760e690d
Maria Feher-Lehrner <maria@feher.at>
2024-11-30 15:56:55 +0100

    update: Äquivalenzklassen für determineInterestRate() und Ergänzung zu ÄK bei Variable initialCapital

--- Zinsenrechner/src/InterestCalculator.java
+++ Zinsenrechner/src/InterestCalculator.java
@@ -1,6 +1,10 @@
 public class InterestCalculator {
 
     //***Method is used in Main to determine the interest rate based on the risk class input***
+
+    //+++risk+++
+    //1) gültige Äquivalenzklasse: Integer Zahlen von 1 bis 3 --> Konkrete Testfälle: 0, 1, 3, 4
+    //2) ungültige Äquivalenzklasse: alles, was sich nicht in diesem Bereich befindet. --> Testfälle von ÄK 1 abgedeckt.
     double determineInterestRate(int risk){
         double interestRate = switch (risk) {
             case 1 -> 2.5;
@@ -15,16 +19,16 @@ public class InterestCalculator {
     //***Method is used in Main to calculate the final max-capital after interest gains.***
 
     //+++initialCapital+++
-    //1) gültige Äquivalenzklasse: 100.00 bis 10000000.00 --> Konkrete Testfälle: 99.99, 100.00, 100.01, 9999999.99, 10000000.00, 10000000.01, mehr als 2 Nachkommastellen werden in der Eingabe aber akzeptiert
-    //2) ungültige Äquivalenzklasse: alles, was sich nicht in diesem Bereich befindet
+    //1) gültige Äquivalenzklasse zweistellige Dezimalzahlen von 100.00 bis 10000000.00 --> Konkrete Testfälle: 99.99, 100.00, 100.01, 9999999.99, 10000000.00, 10000000.01, mehr als 2 Nachkommastellen werden in der Eingabe aber akzeptiert
+    //2) ungültige Äquivalenzklasse: alles, was sich nicht in diesem Bereich befindet --> Testfälle von ÄK 1 abgedeckt.
 
     //+++duration+++
     //1) gültige Äquivalenzklasse: 1 bis 30 Jahre --> Konkrete Testfälle: 0, 1, 2, 29, 30, 31
-    //2) ungültige Äquivalenzklasse: alles, was sich nicht in diesem Bereich befindet.
+    //2) ungültige Äquivalenzklasse: alles, was sich nicht in diesem Bereich befindet. --> Testfälle von ÄK 1 abgedeckt.
 
     //+++interestRate+++
-    //1) gültige Äquivalenzklasse: [2.5, 7, 15] --> Konkrete Testfälle: 2, 2.5, 5, 7, 10.5, 15, 15.5
-    //2) ungültige Äquivalenzklasse: jede Zahl, die NICHT 2.5, 7 oder 15 ist.
+    //1) gültige Äquivalenzklasse: [2.5, 7, 15] --> Konkrete Testfälle: 2.4, 2.5, 5, 7, 10.5, 15, 15.1
+    //2) ungültige Äquivalenzklasse: jede Zahl, die NICHT 2.5, 7 oder 15 ist. --> Testfälle von ÄK 1 abgedeckt.
 
     double calculateEquityAfterInterest(double initialCapital, int duration, double interestRate) {
 
